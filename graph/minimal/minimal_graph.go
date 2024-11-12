c95637b09c9bfb5a65de259e8dc7e04d12897856
Max Gara <maxgara4@gmail.com>
2024-11-11 20:31:04 -0800

    added colors - in process of adding labels for bounds

--- graph/minimal/minimal_graph.go
+++ graph/minimal/minimal_graph.go
@@ -18,34 +18,44 @@ const sep = " "
 
 // read lines of data, when hitting a non-data line check for flag -n indicating a new chart, otherwise add line to current chart
 func main() {
-	const linestart = `<polyline stroke="grey" fill="none" stroke-width="0.7" points="`
+	printSVGs("")
+}
+func printSVGs(instr string) string {
+	// const linestart = `<polyline stroke="grey" fill="none" stroke-width="0.7" points="`
 	const lineend = `"> </polyline>`
-	const svgstart = `<div style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;"><svg width="100%" height="100%" viewBox="0 0 2000 2000" xmlns="http://www.w3.org/2000/svg" overflow="visible">`
+	// const svgstart = `<div style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;"><svg width="100%" height="100%" viewBox="0 0 2000 2000" xmlns="http://www.w3.org/2000/svg" overflow="visible">`
 	const svgend = `</svg></div>`
+	var cols = colors{}
 	var bounds = []float64{10000, 0, 10000, 0}
 	var out string
-	var outtemp string   //store string until it is ready to be added to out
-	outtemp += linestart //add line start but do not add svg start until we have bounds
+	var outtemp string                         //store string until it is ready to be added to out
+	outtemp += printlinestart(cols.newcolor()) //add line start but do not add svg start until we have bounds
 	//read input
-	r := bufio.NewScanner(os.Stdin)
-	var idx int
+	var r *bufio.Scanner
+	if instr != "" {
+		sr := strings.NewReader(instr)
+		r = bufio.NewScanner(sr)
+	} else {
+		r = bufio.NewScanner(os.Stdin)
+	}
+	var idx int // idx of current point in line plot
 	for ok := r.Scan(); ok; ok = r.Scan() {
 		// fmt.Printf(`scanned:"%s"`, r.Text())
 		line := r.Text()
 		t := linetype(line)
-		//begin new line in same SVG
+		//begin new line plot in same SVG
 		if t == EMPTY {
-			outtemp += lineend + linestart
+			outtemp += lineend + printlinestart(cols.newcolor())
 			idx = 0
 			continue
 		}
-		//begin new SVG and new line
+		//begin new SVG and new line plot
 		if t == NEWCHARTFLAG {
-			//
-			outtemp += lineend + svgend
+			cols = colors{} //reset colors
+			outtemp += lineend + printBoundsSVG(bounds) + svgend
 			outtemp = printsvgstart(bounds) + outtemp // prepend svg start tag
 			out += outtemp
-			outtemp = linestart
+			outtemp = printlinestart(cols.newcolor())
 			idx = 0
 			continue
 		}
@@ -54,10 +64,11 @@ func main() {
 		outtemp += printpoint(p)
 		idx++
 	}
-	outtemp += lineend + svgend
+	outtemp += lineend + printBoundsSVG(bounds) + svgend
 	outtemp = printsvgstart(bounds) + outtemp // prepend svg start tag
 	out += outtemp
 	fmt.Print(out)
+	return out
 }
 
 func printpoint(p []float64) string {
@@ -66,7 +77,24 @@ func printpoint(p []float64) string {
 func printsvgstart(b []float64) string {
 	width := b[XMAX] - b[XMIN]
 	height := b[YMAX] - b[YMIN]
-	return fmt.Sprintf(`<div style="width: %s; height: %s; display: flex"><svg viewBox="%d %d %d %d" xmlns="http://www.w3.org/2000/svg">`, `100%`, `100%`, int(b[XMIN]), int(b[YMIN]), int(width), int(height))
+	return fmt.Sprintf(`<div style="width: %s; height: %s; display: flex"><svg viewBox="%d %d %d %d" xmlns="http://www.w3.org/2000/svg"><style>".small {
+      font: italic 13px sans-serif;
+    }"</style>`, `100%`, `100%`, int(b[XMIN]), int(b[YMIN]), int(width), int(height))
+}
+
+func printlinestart(c color) string {
+	return fmt.Sprintf(`<polyline stroke="%v" fill="none" stroke-width="0.7" points="`, c)
+}
+func printBoundsSVG(bounds []float64) string {
+	b := []int{int(bounds[0]), int(bounds[1]), int(bounds[2]), int(bounds[3])}
+	const style = `<style>".small {
+      font: italic 13px sans-serif;
+    }"</style>`
+	return fmt.Sprintf(`%s<text x="2+%d" y="%d-2" class="small">%d,%d</text>
+	 <text x="%d" y="%d" class="small">%d,%d</text>
+	  <text x="%d" y="%d" class="small">%d,%d</text>
+	  `, "",
+		b[XMIN], b[YMIN], b[XMIN], b[YMIN], b[XMAX], b[YMIN], b[XMAX], b[YMIN], b[XMIN], b[YMAX], b[XMIN], b[YMAX])
 }
 
 const (
@@ -75,6 +103,59 @@ const (
 	NEWCHARTFLAG
 )
 
+type colors struct {
+	allcolors []color
+	used      map[color]bool
+}
+
+type color uint32
+
+func (c color) String() string {
+	return fmt.Sprintf("#%06x", uint32(c))
+}
+
+const red color = 0xff0000
+const green color = 0x00ff00
+const blue color = 0x0000ff
+
+func blend(c1, c2 color) color {
+	const rfilter = 0xff0000
+	const gfilter = 0x00ff00
+	const bfilter = 0x0000ff
+	r := (((c1 & rfilter) + (c2 & rfilter)) / 2) & rfilter
+	g := (((c1 & gfilter) + (c2 & gfilter)) / 2) & gfilter
+	b := (((c1 & bfilter) + (c2 & bfilter)) / 2) & bfilter
+	return r | g | b
+}
+func (c *colors) newcolor() color {
+	//initialization case
+	l := len(c.allcolors)
+	if l == 0 {
+		c.allcolors = []color{red, green, blue}
+		c.used = make(map[color]bool)
+		return 0x000000 //start with black
+	}
+	//out of colors case, add new colors
+	if l == len(c.used) {
+		var cnew []color
+		// for each pair of colors a_n, a_n+1 insert new blended color in the middle
+		for i, _ := range c.allcolors {
+			nc := blend(c.allcolors[i], c.allcolors[(i+1)%l]) //new color
+			cnew = append(cnew, c.allcolors[i])               //original color
+			cnew = append(cnew, nc)
+		}
+		c.allcolors = cnew
+		return c.newcolor()
+	}
+	for _, col := range c.allcolors {
+		if !c.used[col] {
+			c.used[col] = true
+			return col
+		}
+	}
+	fmt.Println("hit end of newcolor() - that's not supposed to happen")
+	return red
+}
 func linetype(s string) int {
 	if s == "" {
 		return EMPTY
---
+++ graph/minimal/minimal_graph_test.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestColors(t *testing.T) {
+	var cols = colors{}
+	// var newcols []color
+	// newcols = append(newcols, cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+	fmt.Println(cols.newcolor())
+}
+func TestSvgGen(t *testing.T) {
+	const data = `1 5
+	2 10
+	3 30
+	4 60`
+	fmt.Sprintf(printSVGs(data))
+}
+func TestMultiColorSVG(t *testing.T) {
+	s := ""
+	for _, d := range data {
+		s += fmt.Sprintf("%s\n\n", d)
+	}
+	printSVGs(s)
+}
+func TestPrintBoundsSvg(t *testing.T) {
+	fmt.Printf("bounds:%s\n", printBoundsSVG([]float64{0, 0, 40, 50}))
+}
+
+var data = []string{`1 5
+2 12
+3 25
+4 60
+5 80
+6 45
+7 30
+8 90
+9 70
+10 55`,
+
+	`1 8
+2 15
+3 20
+4 50
+5 60
+6 35
+7 28
+8 85
+9 65
+10 50`,
+
+	`1 10
+2 18
+3 27
+4 62
+5 77
+6 40
+7 36
+8 95
+9 72
+10 58`,
+
+	`1 7
+2 13
+3 24
+4 55
+5 82
+6 47
+7 34
+8 92
+9 68
+10 52`,
+
+	`1 6
+2 14
+3 22
+4 57
+5 75
+6 42
+7 32
+8 88
+9 66
+10 54`,
+
+	`1 9
+2 17
+3 26
+4 61
+5 79
+6 44
+7 29
+8 93
+9 73
+10 53`,
+
+	`1 4
+2 11
+3 23
+4 58
+5 78
+6 41
+7 31
+8 89
+9 69
+10 56`,
+
+	`1 3
+2 16
+3 21
+4 63
+5 76
+6 39
+7 33
+8 87
+9 67
+10 51`,
+}
