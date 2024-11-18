e6c9c199171c6619e65531ff88865ba299a04b0f
Max Gara <maxgara4@gmail.com>
2024-11-17 16:35:37 -0800

    working

--- code-analysis/obs/obs.go
+++ code-analysis/obs/obs.go
@@ -1,39 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
 // object group
-type Obs struct {
-	arr []Ob //shared props
-}
+type Obs []Ob
 
 // object
 type Ob struct {
 	name string
 	val  string
 	p    map[string]Obs //props
+	temp map[string]Obs //temporary props
 }
+
+const MAXMATCH = -1 //control maximum matches per parse
 
 // ex: Ob FileString -> Obg Function [3obs]-> Obg [fname] [1ob]
 func (o *Ob) Parse(name, pattern string) Obs {
-	strs := []string{"place", "holder"}
-	if len(strs) == 0 {
-		return Obs{}
+	p := regexp.MustCompile(pattern)
+	arr := p.FindAllString(o.val, MAXMATCH)
+	var g Obs = Obs{}
+	for i, s := range arr {
+		newname := name + fmt.Sprintf("%v", i)
+		newob := Ob{name: newname, val: s, p: make(map[string]Obs)}
+		g = append(g, newob)
 	}
-	// var g Obs = Obs{p: make(map[string]bool)}
-	// for i, v := range strs {
-	// 	newname := name + fmt.Sprintf("%v", i)
-	// 	newob := Ob{name: newname, val: v, p: make(map[string]Obs)}
-	// }
-	// o.p[name] = g
-	// Ob.p[name] = Obg
+	o.p[name] = g
+	return g
 }
 
-func (o Obs) ParseAll(name, pattern string) Obs {
-	// TODO
-	return Obs{}
+func (o Obs) ParseEach(name, pattern string) Obs {
+	var all Obs
+	for _, v := range o {
+		newobs := v.Parse(name, pattern)
+		all = append(all, newobs...)
+	}
+	return all
 }
 
 // read prop p for all x in o and return set of all x.y.val
 func (o Obs) Get(p string) []string {
 	//TODO
 	return nil
+}
+
+func (o Obs) Push(p Obs) []string {
+
 }
