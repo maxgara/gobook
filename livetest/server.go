afd72c5e39229f9802534dc6bdede0726dfe1fd3
Max Gara <maxgara4@gmail.com>
2024-11-27 12:03:09 -0800

    making progress.I have the basic idea of a web interface for viewing changes implemented. Currently the caller has to implement methods for printing the object, processing text as a command to the object, and initializing the object into a base state. This may be fine actually, but I would ideally like to make this a little simpler and support the objects original methods, or some subset, directly; without a text processing wrapper needing to be written each time. I think I may write a basic function to register a new type as a wn, specifying which methods to include by passing them as variables alongside tags for user input args. This would require using an empty interface type and doing some type assertions, but may be possible. otherwise I guess I need reflection. I would also like to add a function to register a special method for getting child nodes, which would be treated as wms themselves. Then I can let the user pick which one to use for a method call, which would be cool for trees and the like.

--- livetest/server.go
+++ livetest/server.go
@@ -3,19 +3,22 @@ package live
 import (
 	"fmt"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
-	"net/url"
-	"strings"
 )
 
-var templ *template.Template
+var doctempl *template.Template
+var statetempl *template.Template
+var textareatempl *template.Template
 
-// web string interface. Any WebModule implementation wm should be a reference type.
-// WString should return an HTML string that can be displayed in a <div> element within a webpage. Should not modify wm.
-// WInput should process a line of text input in a way that is transparent to the web user.
-// WInit should initialize a wm implementation state in a way that is invarient to previous state. (maybe start with make(<wm>))
+const docTemplFile = "/Users/maxgara/Desktop/go-code/gobook/workspace/livetest/basic.tmpl"
+const stateTemplFile = "/Users/maxgara/Desktop/go-code/gobook/workspace/livetest/state.tmpl"
+const textareaTemplFile = "/Users/maxgara/Desktop/go-code/gobook/workspace/livetest/textarea.tmpl"
+
+// web module interface. Implementation should not be a reference type, as the value of the wm
+// used to call Live() is used as an initialization constant. WString should return an HTML string that can
+// be displayed in a <div> element within a webpage. Should not modify wm.
+// WInput should process a line of text input in a way that is transparent to the user.
 
 type WebModule interface {
 	WString() string
@@ -26,57 +29,84 @@ type WebModule interface {
 type tempstruct struct {
 	Webstring string
 }
+
+// input state
 type state struct {
-	calls []string //input from current state of webpage
+	calls map[string]string //map of [input_id]input
+	order []string          //[]input_id
 }
 
-// create a WebModule state equivalent corresponding to st
+type uicomp struct {
+	Id string //html id
+}
+
+// get system state for input state
 func runstate(wm WebModule, st state) {
-	wm.WInit()
-	for _, str := range st.calls {
-		wm.WInput(str)
+	for _, id := range st.order {
+		wm.WInput(st.calls[id])
 	}
 }
 
 // process input into calls and add to struct
-func addcall(stt *state, str string) {
-	fmt.Printf("addcall: str=%v\n", str)
-	str, _ = url.QueryUnescape(str)
-	str = strings.TrimPrefix(str, "input=")
-	stt.calls = append(stt.calls, str)
+func statechange(stt *state, call string, caller string) {
+	fmt.Printf("state change: str=%v\n", call)
+	//add new caller if missing
+	if _, ok := stt.calls[caller]; !ok {
+		stt.order = append(stt.order, caller)
+	}
+	stt.calls[caller] = call //update call in state
 }
 
 // serve object's WString within a basic HTML template, server address is localhost:8000.
-func liveprint(target WebModule) {
-	var st state
-	//serve current state to user
+func liveprint(wm WebModule) {
+	//initialize
+	wm.WInit()
+	st := state{calls: make(map[string]string)}
+	var ui []uicomp
+	//serve document
 	handler := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("handler called. state: %v\n", st)
-		runstate(target, st)
-		fmt.Printf("after runstate, webmodule obj= %v\n", target)
-		obj := tempstruct{Webstring: target.WString()}
-		fmt.Printf("getting WString from webmodule:%v\n", target.WString())
-		templ.Execute(w, obj) //write html to resp
+		fmt.Printf("doc root handler called.\n")
+		doctempl.Execute(w, struct{}{}) //write html to resp
+		fmt.Printf("sent doc, handler done.\n")
+	}
+	//serve current state to user
+	readHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("read handler called. state is %v\n", st)
+		wm.WInit()
+		runstate(wm, st) //get system state for input state
+		fmt.Printf("after runstate, webmodule obj= %v\n", wm)
+		obj := tempstruct{Webstring: wm.WString()}
+		fmt.Printf("getting WString from webmodule:%v\n", wm.WString())
+		statetempl.Execute(w, obj) //write html to resp
 		fmt.Printf("sent resp. handler done.\n")
 	}
 	//update state to match user
 	inputHandler := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("input handler called. current (unsynced) server state :%v\n", st)
-		bytes, _ := io.ReadAll(r.Body)
-		callstr := string(bytes)
-		st = state{} // reset state. Eventually may not do this if input HTTP requests do not include entire user state.
-		addcall(&st, callstr)
+		r.ParseForm()
+		callstr := r.Form.Get("input")
+		trigger := r.Header.Get("HX-Trigger")
+		statechange(&st, callstr, trigger)
 		fmt.Printf("input handled. state:%v\n", st)
-		fmt.Printf("input handler calling handler.\n")
-		handler(w, r) // send back updated state to user
+		fmt.Printf("input handler calling read handler.\n")
+		readHandler(w, r) // send back updated state to user
 	}
-	var err error
-	templ, err = template.ParseFiles("/Users/maxgara/Desktop/go-code/gobook/workspace/livetest/basic.tmpl")
-	if err != nil {
-		log.Fatal(err)
+	uiUpdateHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("ui update handler called: current ui: %v\n", ui)
+		r.ParseForm()
+		_ = r.Form.Get("HX-Trigger")
+		newcomp := uicomp{"input" + fmt.Sprint(len(ui))}
+		ui = append(ui, newcomp)
+		textareatempl.Execute(w, newcomp)
 	}
+
+	doctempl = template.Must(template.ParseFiles(docTemplFile))
+	statetempl = template.Must(template.ParseFiles(stateTemplFile))
+	textareatempl = template.Must(template.ParseFiles(textareaTemplFile))
 	http.HandleFunc("/input", inputHandler)
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/output", readHandler)
+	http.HandleFunc("/uiupdate", uiUpdateHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 func LivePrint(target any) {
