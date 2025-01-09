df0597ba68f93c352edbda891380aa623779b9ae
Max Gara <maxgara4@gmail.com>
2025-01-09 14:04:45 -0800

    wip

--- graph/quickserve/main.go
+++ graph/quickserve/main.go
@@ -8,17 +8,22 @@ import (
 )
 
 func main() {
-	http.HandleFunc("localhost:8000", handler)
 	execfp := os.Args[1]
 	readfp := os.Args[2]
-	handler := func(http.ResponseWriter, *http.Request) {
-		exec.Command(execfp).Run()
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("exec command [%v]\n", execfp)
+		err := exec.Command(execfp).Run()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 		b, err := os.ReadFile(readfp)
 		if err != nil {
 			fmt.Println(err)
-			return
+			os.Exit(1)
 		}
-
+		w.Write(b)
 	}
-
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("localhost:8001", nil)
 }
---
+++ graph/quickserve/script.sh
@@ -0,0 +1,1 @@
+echo "goodbye world" > temp.html
