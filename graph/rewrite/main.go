fe86c59421b9ce00dd2de85031ba7df639e34a02
Max Gara <maxgara4@gmail.com>
2025-01-14 19:52:21 -0800

    mostly working version

--- graph/rewrite/main.go
+++ graph/rewrite/main.go
@@ -34,6 +34,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"math"
 	"os"
@@ -41,40 +42,12 @@ import (
 	"strings"
 )
 
-// type svg struct {
-// 	sset                   [][]float64 //data series
-// 	Xmin, Xmax, Ymin, Ymax float64     //bounds for SVG viewbox
-// }
-
 const POLYWIDTH = 0.1 //polyline stroke width
 
-// data types
-const (
-	DATA = iota
-	TEXT
-	FLAGS
-)
-
-// flags
-const (
-	NEWCHART = 0b1 << iota
-	PARALLEL
-)
-
 type parser struct {
-	s         string
-	lines     []string
-	t         int //type of input processed
-	dcols     int // # of data columns
-	pagetitle string
-	title     string //svg title
-	text      string
-	flags     int
-	data      [][]float64
-	err       error
-	css       string
-	readback  []byte
-	secs      []sectionVars
+	s     string
+	lines []string
+	sec   section
 }
 
 func newParser(r io.Reader) parser {
@@ -82,22 +55,25 @@ func newParser(r io.Reader) parser {
 	if err != nil {
 		panic(err)
 	}
-	return parser{s: string(s)}
+	lines := strings.Split(string(s), "\n")
+	return parser{s: string(s), lines: lines}
 }
 
+// parse all input from r and print document to stdout
 func parseAllAndRender(r io.Reader) {
 	p := newParser(r)
+	var secs []section
 	for p.parse() {
-		//continue
+		secs = append(secs, p.sec)
 	}
 	first := true
 	var ingrid bool //do we need to close a grid?
 	b := newDocBuilder(os.Stdout)
-	for _, sec := range p.secs {
-		if sec.gridFlag != 0 {
+	for _, sec := range secs {
+		if sec.flags.grid != 0 {
 			ingrid = true
 		}
-		encodeSection(sec.text, sec.data, sec.d1Flag, sec.d2Flag, sec.dxFlag, sec.gridFlag, b, first, ingrid)
+		encodeSection(sec.text, sec.data, sec.headers, sec.flags, b, first, ingrid)
 		first = false
 	}
 	endEncoding(b, ingrid)
@@ -113,7 +89,7 @@ func endEncoding(b *docBuilder, gfset bool) {
 
 // read specified line
 func (p *parser) readline(lidx int) (l string, words []string, ok bool) {
-	if lidx >= len(p.lines) {
+	if lidx >= len(p.lines) || lidx < 0 {
 		return "", nil, false
 	}
 	l = strings.Trim(p.lines[lidx], " \t\r")
@@ -122,19 +98,23 @@ func (p *parser) readline(lidx int) (l string, words []string, ok bool) {
 	return
 }
 
-// parses a section of input into a sectionVars struct
-// each call to parse reads until a break in the data series
-// sets flags, titles, and text section properties for p
-// returns false when parsing is complete, either due to error or end of input
-func (p *parser) parse() bool {
-	if p.lines == nil {
-		p.lines = strings.Split(p.s, "\n")
+// parses a section of input into a section struct
+// each call to parse reads until a break in the data series(s)
+// sets flags, titles, and text section properties for p.sec
+// returns false only after input string has been completely parsed.
+// if parse() returns false, no data is present in p.sec and therefore
+// the last successful call to parse will be after data is already done being parsed and len(p.lines) == 0.
+func (p *parser) parse() (ok bool) {
+	if len(p.lines) == 0 {
+		return false
 	}
 	var lidx int
 	var data [][]float64
+	var text []string
 	var flagstrs []string
-	var done bool
+	var done bool // end of input
 	//parse non-data first
+TLOOP:
 	for !done {
 		l, words, ok := p.readline(lidx)
 		switch {
@@ -144,94 +124,141 @@ func (p *parser) parse() bool {
 			lidx++ //blank lines ignored in text mode
 		case isdata(words):
 			data = make([][]float64, len(words))
-			break
+			break TLOOP // do not increment lidx, allow processing of l as data
 		case l[0] == '-':
-			flagstrs = append(flagstrs, words...) //add to flags list, continue
+			flagstrs = append(flagstrs, words...)
 			lidx++
 		default:
-			p.text += l
+			text = append(text, l)
 			lidx++
-			continue
 		}
 	}
+	hlidx := lidx - 1      //header line idx
+	htidx := len(text) - 1 //header text idx
 	// then parse data
+DLOOP:
 	for !done {
 		l, words, ok := p.readline(lidx)
 		switch {
 		case !ok:
 			done = true
 		case len(l) == 0:
-			break //blank lines mean end of series in data mode
+			break DLOOP //blank lines mean end of series in data mode
+		case !isdata(words):
+			break DLOOP // do not increment lidx, allow processing of l as text
 		default:
 			for i, w := range words {
 				d, err := strconv.ParseFloat(w, 64)
 				if err != nil {
-					break
+					break DLOOP // same as !isdata case
 				}
-				data[i] = append(data[i], d) //add ith data point to data series i
+				data[i] = append(data[i], d)
 			}
 			lidx++
-			continue //next line
 		}
 	}
-
-	//parse flags
+	//parse headers if applicable
+	var headers []string
+	l, h, ok := p.readline(hlidx)
+	if ok && len(l) != 0 && l[0] != '-' && len(data) != 0 {
+		headers = h
+		//drop headers from text
+		haft := text[htidx:]
+		if len(haft) > 1 {
+			copy(haft, haft[1:])
+		}
+		text = text[:len(text)-1]
+	}
+	//parse collected flags
 	flags := flag.NewFlagSet("svgparseflags", flag.PanicOnError)
-	d1Flag := flags.Bool("d1", true, "one coordinate series parsing")
-	d2Flag := flags.Bool("d2", false, "two coordinate series parsing")
-	dxFlag := flags.Bool("dx", false, "shared x value")
-	gridFlag := flags.Int("grid", 0, "grid column count")
+	sf := sectionFlags{}
+	flags.BoolVar(&sf.d1, "d1", true, "one coordinate series parsing")
+	flags.BoolVar(&sf.d2, "d2", false, "two coordinate series parsing")
+	flags.BoolVar(&sf.dx, "dx", false, "shared x value")
+	flags.IntVar(&sf.grid, "grid", 0, "grid column count")
+	flags.StringVar(&sf.title, "title", "", "section title")
+	flags.StringVar(&sf.pageTitle, "pagetitle", "Graph Output", "page title")
+	flags.StringVar(&sf.xl, "xlabel", "x-axis", "x axis label")
+	flags.StringVar(&sf.yl, "ylabel", "y-axis", "y axis label")
 
 	flags.Parse(flagstrs)
-	//handle result of parsing this block
-	sec := sectionVars{text: p.text, data: data, d1Flag: *d1Flag, d2Flag: *d2Flag, dxFlag: *dxFlag, gridFlag: *gridFlag}
-	p.secs = append(p.secs, sec)
-	p.lines = p.lines[lidx:]
+
+	//return parsed section
+	p.sec = section{text: strings.Join(text, "\n"), data: data, headers: headers, flags: sf}
 	if done {
-		return false
+		p.lines = nil
+	} else {
+		p.lines = p.lines[lidx:]
 	}
-	return true
+	return true //ok
 }
 
-const (
-	D1_FLAG = 0b1 << iota
-	D2_FLAG
-	DX_FLAG
-)
-
-type sectionVars struct {
-	text     string
-	data     [][]float64
-	d1Flag   bool
-	d2Flag   bool
-	dxFlag   bool
-	gridFlag int
+type sectionFlags struct {
+	d1        bool
+	d2        bool
+	dx        bool
+	grid      int
+	title     string
+	xl        string //x axis label
+	yl        string //y axis label
+	pageTitle string
+}
+type section struct {
+	text    string
+	data    [][]float64
+	headers []string
+	flags   sectionFlags
 }
 
 // convert parsed data to document format
-func encodeSection(text string, data [][]float64, d1Flag, d2Flag, dxFlag bool, gridFlag int, b *docBuilder, first bool, ingrid bool) {
-	b.writeText(text)
-	if len(data) == 0 {
-		return
+func encodeSection(text string, data [][]float64, h []string, f sectionFlags, b *docBuilder, first bool, ingrid bool) {
+	if first {
+		b.startDoc()
+		b.writePageTitle(f.pageTitle)
 	}
+	if f.grid != 0 {
+		b.startGrid(f.grid) // closed by endEncoding func
+		ingrid = true
+	}
+	if ingrid {
+		b.startGridElem()
+	}
+	// if f.title != "" {
+	// 	b.writeTitle(f.title)
+	// }
+	b.writeText(text)
+	if len(data) != 0 {
+		encodeSVG(data, h, f, b)
+	}
+	if ingrid {
+		b.endGridElem()
+	}
+}
+
+// build the SVG document, including svg-title and plot legend
+func encodeSVG(data [][]float64, h []string, f sectionFlags, b *docBuilder) {
+
 	//assign pairs of data columns to contain x,y values of series based on flags
 	var spairs [][2][]float64
+	if len(data) == 0 {
+
+	}
 	idxs := make([]float64, len(data[0])) //new index data series for convenience
 	for i := range len(data[0]) {
 		idxs[i] = float64(i)
 	}
 	switch {
-	case d2Flag && !dxFlag:
+	case f.d2 && !f.dx:
 		for i := 0; i < len(data); i += 2 {
 			spair := [2][]float64{data[i], data[i+1]}
 			spairs = append(spairs, spair)
 		}
-	case dxFlag:
+	case f.dx:
 		for i := 1; i < len(data); i++ {
 			spair := [2][]float64{data[0], data[i]}
 			spairs = append(spairs, spair)
 		}
-	case d1Flag:
+	case f.d1:
 		for _, s := range data {
 			spair := [2][]float64{idxs, s}
 			spairs = append(spairs, spair)
@@ -252,20 +279,16 @@ func encodeSection(text string, data [][]float64, d1Flag, d2Flag, dxFlag bool, g
 			ymax = max(ymax, y)
 		}
 	}
-	//draw
-	if first {
-		b.startDoc()
-	}
-	if gridFlag != 0 {
-		b.startGrid(gridFlag) // closed by endEncoding func
-	}
-	if ingrid {
-		b.startGridElem()
-	}
 	bounds := [4]float64{xmin, ymin, xmax - xmin, ymax - ymin}
-	b.startSVG("temporary svg title", bounds)
-	for _, s := range spairs {
-		b.startPoly(0.1)
+	//fill in missing headers
+	for len(h) < len(spairs) {
+		nh := fmt.Sprintf("series # %v", len(h)+1)
+		h = append(h, nh)
+	}
+	//draw
+	b.startSVG(f.title, bounds, f.xl, f.yl)
+	for n, s := range spairs {
+		b.startPoly(0.1, h[n]) //header #n mapped to spair #n
 		for i := range s[0] {
 			x := s[0][i]
 			y := s[1][i]
@@ -274,11 +297,8 @@ func encodeSection(text string, data [][]float64, d1Flag, d2Flag, dxFlag bool, g
 		b.endPoly()
 	}
 	b.endSVG()
-	if ingrid {
-		b.endGridElem()
-	}
+	b.writeLabels()
 }
-
 func isdata(words []string) bool {
 	datachars := []byte("-0123456789.E")
 	for _, w := range words {
@@ -300,6 +320,8 @@ func isdata(words []string) bool {
 }
 
 func main() {
-	test := "-grid=2\n10 10\n20 10\n30 40\n\n0 0\n 1 2\n0 0\n-0.31 -0.5"
-	parseAllAndRender(strings.NewReader(test))
+
+	// test := "-grid=2\njane bob\n10 10\n20 10\n30 40\n\nsammy ethyl\n0 0\n 1 2\n0 0\n-0.31 -0.5"
+	// parseAllAndRender(strings.NewReader(test))
+	parseAllAndRender(os.Stdin)
 }
--- graph/rewrite/svg.go
+++ graph/rewrite/svg.go
@@ -20,12 +20,37 @@ var style string
 
 // document is made up of a title, and grids of text, svg, and svg-label elements
 type docBuilder struct {
-	gridcols int //grid column count
-	grididx  int
-	cidx     int //palette color index
-	w        io.Writer
+	gridcols   int //grid column count
+	grididx    int
+	cidx       int //palette color index
+	labelstack []string
+	xl         string
+	yl         string
+	w          io.Writer
 }
 
+const (
+	RMASK = 0xff0000 >> (8 * iota)
+	GMASK
+	BMASK
+)
+
+// darken rgb color by given percent
+func darken(col uint32, per uint32) uint32 {
+	adj := func(k uint32) uint32 {
+		return k - (per*k)/100
+	}
+	var newcol uint32
+	for i := range 3 {
+		bsh := 8 * i
+		bm := uint32(0xff0000) >> bsh
+		cmp := col & bm >> (16 - bsh)
+		c := adj(cmp) << (16 - bsh)
+		newcol |= c
+	}
+	fmt.Printf("adjusted %x to %x (%v%%)\n", col, newcol, per)
+	return newcol
+}
 func newDocBuilder(w io.Writer) *docBuilder {
 	b, err := os.ReadFile(style_file)
 	if err != nil {
@@ -35,10 +60,27 @@ func newDocBuilder(w io.Writer) *docBuilder {
 	return &docBuilder{w: w}
 }
 
-// write a document portion to w; internal use
+// write format string with args to w; svg.go internal use
 func (d *docBuilder) writef(fstr string, args ...any) {
 	s := fmt.Sprintf(fstr, args...)
 	d.w.Write([]byte(s))
+}
+
+// write a block of labels below svg
+func (d *docBuilder) writeLabels() {
+	d.writef("<div class=labels>")
+	//reset cidx to match color at the bottom of the label stack
+	d.cidx = d.cidx - len(d.labelstack)
+	for d.cidx < 0 {
+		d.cidx += len(pal)
+	}
+	//write labels
+	for _, s := range d.labelstack {
+		d.writef(`<div id=label style="background-color: #%x">%v</div>`, pal[d.cidx], s)
+		d.cidx++
+	}
+	d.writef("</div>")
+	d.labelstack = nil
 }
 func (d *docBuilder) startDoc() {
 	d.writef(STARTDOC_FSTR, style)
@@ -50,19 +92,22 @@ func (d *docBuilder) writeText(s string) {
 	d.writef(`<div class="text-block">%v</div>`, s)
 }
 func (d *docBuilder) writeTitle(s string) {
-	d.writef("<div id=title>%v</div>", s)
+	d.writef(`<div class="title">%v</div>`, s)
+}
+func (d *docBuilder) writePageTitle(s string) {
+	d.writef(`<div class="pagetitle">%v</div>`, s)
 }
 func (d *docBuilder) startGrid(cols int) {
 	d.gridcols = cols
 	d.grididx = 0
-	d.writef("<div class=grid><div class=grid-row>")
+	d.writef(`<div class=grid><div class="grid-row">`)
 }
 func (d *docBuilder) endGrid() {
 	d.writef("</div></div>")
 }
 func (d *docBuilder) startGridElem() {
 	if d.grididx != 0 && d.grididx%d.gridcols == 0 {
-		d.writef("</div><div class=grid-row>")
+		d.writef(`</div><div class="grid-row">`)
 	}
 	d.writef(`<div class="grid-elem">`)
 	d.grididx++
@@ -72,15 +117,18 @@ func (d *docBuilder) endGridElem() {
 }
 
 // start SVG element with given title and viewbox bounds
-func (d *docBuilder) startSVG(title string, view [4]float64) {
-	d.writef(SVGSTART_FSTR, title, view[0], view[1], view[2], view[3])
+func (d *docBuilder) startSVG(title string, view [4]float64, xaxis string, yaxis string) {
+	d.yl = yaxis
+	d.xl = xaxis
+	d.writef(SVGSTART_FSTR, title, yaxis, view[0], view[1], view[2], view[3])
 }
 func (d *docBuilder) endSVG() {
-	d.writef(SVGEND_FSTR)
+	d.writef(SVGEND_FSTR, d.xl)
 }
 
-// start a polyline with given stroke width
-func (g *docBuilder) startPoly(width float64) {
+// start a polyline with given stroke width and label, which can be written later
+func (g *docBuilder) startPoly(width float64, label string) {
+	g.labelstack = append(g.labelstack, label)
 	stroke := pal[g.cidx]
 	g.cidx = (g.cidx + 1) % len(pal)
 	g.writef(POLYSTART_FSTR, stroke, width)
@@ -94,9 +142,9 @@ func (g *docBuilder) vertex(x, y float64) {
 
 const (
 	POLY_STROKE_WIDTH_DEFAULT = 0.5
-	SVGSTART_FSTR             = `<div class="svg-container"><div class="svg-title">%v</div><svg viewBox="%v %v %v %v" preserveAspectRatio="none"
+	SVGSTART_FSTR             = `<div class="svg-container"><div class="svg-title">%v</div><div class="yaxislabel">%v</div><div class="svg-axisalignment"><svg viewBox="%v %v %v %v" preserveAspectRatio="none"
 			xmlns="http://www.w3.org/2000/svg">`
-	SVGEND_FSTR    = `</svg></div>`
+	SVGEND_FSTR    = `</svg><div class="xaxislabel">%v</div></div></div>`
 	POLYSTART_FSTR = `<polyline stroke="#%x" fill="none" stroke-width="%v"
 				points="`
 	POLYEND_FSTR = `">
