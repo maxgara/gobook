ac4c5cddb962f57010542d84552d4387843129df
Max Gara <maxgara4@gmail.com>
2025-02-16 13:43:36 -0800

    voxel-y version, barycentric triangle filling not working yet

--- sdl/tinyrenderer/lesson1.go
+++ sdl/tinyrenderer/lesson1.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"runtime"
 	"runtime/pprof"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/veandco/go-sdl2/sdl"
@@ -16,38 +19,161 @@ const (
 )
 
 var file *os.File
+var fileVerts []vert
+var fileFaces []face
+var filebounds struct {
+	xmin, xmax, ymin, ymax float64
+}
 
 func main() {
+
+	// b := bary(v, w, [2]float64{0.15, 0.1})
+
+	// fmt.Printf("b=<%f %f>\n\n\n", b[0], b[1])
+	// fmt.Println(b)
+	// <-time.After(1 * time.Hour)
 	// file, _ = os.Create("cpuprofile")
 	// pprof.StartCPUProfile(file)
 	// defer pprof.StopCPUProfile()
 
 	// file, _ = os.Create("memprofile")
+	fileVerts, fileFaces = loadobjfile("african_head.obj")
+	// fmt.Println(fileVerts)
+	// fmt.Println("faces")
+	// fmt.Println(fileFaces)
+	// fmt.Println("bounds")
+	// fmt.Println(filebounds)
 	setupAndDraw()
 }
 
-func loadobjfile(filename string) {
-	os.ReadFile("african_head.obj")
+type vert [3]float64
+type face = [3]int
 
+// load vertex from string
+func loadVertex(s string, verts *[]vert) {
+	fs := strings.Fields(s)
+	var vt vert
+	for i, coordstr := range fs[1:] {
+		coord, err := strconv.ParseFloat(coordstr, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		vt[i] = coord
+	}
+	//adjust bounds
+	b := &filebounds
+	b.xmax = max(b.xmax, vt[0])
+	b.xmin = min(b.xmin, vt[0])
+	b.ymax = max(b.ymax, vt[1])
+	b.ymin = min(b.ymin, vt[1])
+	*verts = append(*verts, vt)
+}
+func loadface(s string, faces *[]face) {
+	fs := strings.Fields(s)
+	var f face
+	for i, field := range fs[1:] {
+		idxstr := strings.Split(field, "/")[0]
+		fidx, err := strconv.ParseInt(idxstr, 10, 32)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f[i] = int(fidx)
+	}
+	*faces = append(*faces, f)
+}
+func loadobjfile(filename string) (verts []vert, faces []face) {
+	f, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := strings.Split(string(f), "\n")
+
+	for _, v := range s {
+		switch {
+		case strings.HasPrefix(v, "v "):
+			loadVertex(v, &verts)
+		case strings.HasPrefix(v, "f "):
+			loadface(v, &faces)
+		}
+	}
+	// fmt.Println(verts)
+	return verts, faces
+}
+
+// convert vertices to pixels
+func vtop(v vert) (p [3]int) {
+	for i := range 3 {
+		vadj := ((v[i] + 1) / 2) //adjust so v >= 0
+		scale := min(width, height)
+		p[i] = int(float64(scale) * vadj)
+	}
+	return p
+}
+func vtop2(v vert) (p [2]int) {
+	for i := range 2 {
+		vadj := ((v[i] + 1) / 2) //adjust so v >= 0
+		scale := min(width, height)
+		p[i] = int(float64(scale) * vadj)
+	}
+	return p
+}
+
+// rotate around +y axis by t(heta) radians
+func rotateVert(v vert, t float64) vert {
+	st := math.Sin(t)
+	ct := math.Cos(t)
+	x := v[0]
+	y := v[1]
+	z := v[2]
+	x1 := x*ct + z*st
+	y1 := y
+	z1 := z*ct - x*st
+	return vert{x1, y1, z1}
+}
+func update() {
+	for i := range fileVerts {
+		v := fileVerts[i]
+		v = rotateVert(v, 0.1)
+		fileVerts[i] = v
+	}
 }
 
 // bgra
 func draw(pixels []byte) {
 	//simpler line drawing func for convenience
-	line := func(x0, y0, x1, y1 int) {
-		DrawLine(x0, y0, x1, y1, pixels)
-	}
-	line(width/3, height, width, height/3)
-	DrawLine(0, 0, width, height, pixels)
-	DrawLine(0, 0, width/2, height, pixels)
-	DrawLine(0, 0, width, height/2, pixels)
-	DrawLine(0, height, width, 0, pixels)
-	// putpixel(width/2, height/2, 0xffffff00, pixels)
-	// line(height*1/6, 0xff000000, pixels)
-	// line(height*2/6, 0x00ff0000, pixels)
-	// line(height*3/6, 0x0000ff00, pixels)
-	// line(height*4/6, 0x000000ff, pixels)
+	// line := func(x0, y0, x1, y1 int) {
+	// 	DrawLine(x0, y0, x1, y1, pixels)
+	// }
 
+	// v0 := [2]int{0, 0}
+	// v1 := [2]int{width / 2, height / 3}
+	// v2 := [2]int{0, height}
+	// fillTriangle(v0, v1, v2, pixels)
+
+	// vline := func(vertex0, vertex1 vert) {
+	// 	p0 := vtop(vertex0)
+	// 	p1 := vtop(vertex1)
+	// 	DrawLine(p0[0], p0[1], p1[0], p1[1], pixels)
+	// }
+	fill3 := func(vt0, vt1, vt2 vert) {
+		p0, p1, p2 := vtop2(vt0), vtop2(vt1), vtop2(vt2)
+		fillTriangle(p0, p1, p2, pixels)
+	}
+
+	// u := vert{0, 0, 0}
+	// v := vert{0.2, 0, 0}
+	// w := vert{0.1, 0.1, 0}
+	// U, V, W := vtop2(u), vtop2(v), vtop2(w)
+	// fillTriangle(U, V, W, pixels)
+
+	for _, f := range fileFaces {
+		vidx0, vidx1, vidx2 := f[0], f[1], f[2]
+		v0, v1, v2 := fileVerts[vidx0-1], fileVerts[vidx1-1], fileVerts[vidx2-1]
+		// vline(v0, v1)
+		// vline(v1, v2)
+		// vline(v2, v0)
+		fill3(v0, v1, v2)
+	}
 }
 func DrawLine(x0, y0, x1, y1 int, pixels []byte) {
 	var color uint32 = 0x0000ff00
@@ -89,7 +215,7 @@ func DrawLine(x0, y0, x1, y1 int, pixels []byte) {
 	}
 }
 func putpixel(x, y int, color uint32, pixels []byte) {
-	if x >= width || y >= height {
+	if x >= width || y >= height || x < 0 || y < 0 {
 		return
 	}
 	idx := 4 * (x + width*y)
@@ -135,7 +261,7 @@ func setupAndDraw() {
 	var done bool
 	var loopsPerSec float64
 	go func() {
-		<-time.After(time.Second * 60)
+		<-time.After(time.Second * 120)
 
 		dur = time.Since(start)
 		done = true
@@ -153,6 +279,7 @@ func setupAndDraw() {
 		surf.Lock()
 		pix := surf.Pixels()
 		// drawLines(arr, pix)
+		update()
 		draw(pix)
 		surf.Unlock()
 		first = !first
@@ -187,47 +314,136 @@ func setupAndDraw() {
 			}
 		}
 		sdl.Delay(100)
-	}
 
+	}
 }
-func drawLines(arr []sdl.Point, pix []byte) {
-	// fmt.Println(arr)
 
-	setp := func(p sdl.Point) {
-		x := p.X
-		y := p.Y
-		if x < 0 || x >= width || y < 0 || y >= width {
-			return
-		}
-		idx := x*4 + y*width*4
-		pix[idx] = 255
-	}
-	var N = 100
-	drawLine := func(x0, y0, x1, y1 int32) {
-		for i := range N {
-			perc := (100 * i) / (N - 1)
-			// fmt.Println("percent=%v\n", perc)
-			x := (x0*(100-int32(perc)) + x1*int32(perc)) / 100
-			y := (y0*(100-int32(perc)) + y1*int32(perc)) / 100
-			p := sdl.Point{x, y}
-			setp(p)
-		}
-	}
-	drawLineSDLPoint := func(p0, p1 sdl.Point) {
-		drawLine(p0.X, p0.Y, p1.X, p1.Y)
-	}
-	for i := 0; i < len(arr); i += 4 {
-		a, b, c, d := arr[i], arr[i+1], arr[i+2], arr[i+3]
-		drawLineSDLPoint(a, b)
-		drawLineSDLPoint(b, c)
-		drawLineSDLPoint(c, d)
-		drawLineSDLPoint(d, a)
-	}
-	// for i := 0; i < width; i++ {
-	// 	drawPoint(sdl.Point{X: int32(i), Y: height / 2})
+// fmt.Println(arr)
+// }
+// 	setp := func(p sdl.Point) {
+// 		x := p.X
+// 		y := p.Y
+// 		if x < 0 || x >= width || y < 0 || y >= width {
+// 			return
+// 		}
+// 		idx := x*4 + y*width*4
+// 		pix[idx] = 255
+// 	}
+// var N = 100
+// drawLine := func(x0, y0, x1, y1 int32) {
+// 	for i := range N {
+// 		perc := (100 * i) / (N - 1)
+// 		// fmt.Println("percent=%v\n", perc)
+// 		x := (x0*(100-int32(perc)) + x1*int32(perc)) / 100
+// 		y := (y0*(100-int32(perc)) + y1*int32(perc)) / 100
+// 		p := sdl.Point{x, y}
+// 		setp(p)
+// 	}
+// }
+// drawLineSDLPoint := func(p0, p1 sdl.Point) {
+// 	drawLine(p0.X, p0.Y, p1.X, p1.Y)
+// }
+// for i := 0; i < len(arr); i += 4 {
+// 	a, b, c, d := arr[i], arr[i+1], arr[i+2], arr[i+3]
+// 	drawLineSDLPoint(a, b)
+// 	drawLineSDLPoint(b, c)
+// 	drawLineSDLPoint(c, d)
+// 	drawLineSDLPoint(d, a)
+// }
+// // for i := 0; i < width; i++ {
+// // 	drawPoint(sdl.Point{X: int32(i), Y: height / 2})
 
-	// }
-	drawLine(0, height/2, width-1, height/2)
+// // }
+// drawLine(0, height/2, width-1, height/2)
+// }
+type Box struct {
+	xmin int
+	ymin int
+	xmax int
+	ymax int
+}
+
+func bound(points [][2]int) Box {
+	xmax, ymax := -1000, -1000
+	xmin, ymin := 1000, 1000
+	for _, p := range points {
+		xmin, ymin = min(xmin, p[0]), min(ymin, p[1])
+		xmax, ymax = max(xmax, p[0]), max(ymax, p[1])
+	}
+	return Box{xmin: xmin, ymin: ymin, xmax: xmax, ymax: ymax}
+}
+
+// fill in a triangle between pixels p{0,1,2}
+func fillTriangle(p0, p1, p2 [2]int, pix []byte) {
+	// u := vdiff(p1, p0)
+	// v := vdiff(p2, p0)
+	box := bound([][2]int{p0, p1, p2})
+	for i := box.xmin; i < box.xmax; i++ {
+		for j := box.ymin; j < box.ymax; j++ {
+			// fmt.Printf("looping: i=%v j=%v box=%v\n", i, j, box)
+			// X := [2]float64{float64(i - box.xmin), float64(j - box.ymin)}
+			// b := bary(u, v, X)
+			// fmt.Printf("\tbary= %v %v", b[0], b[1])
+			// mag := math.Hypot(b[0], b[1])
+			// mag := b[0] + b[1]
+			//check if b is in triangle made up of vectors u,v
+			// if b[0] < 0 || b[1] < 0 || mag > 1 {
+			// 	continue
+			// }
+			// fmt.Printf("pass check, in triangle")
+			putpixel(i, j, 0x0000ff00, pix)
+		}
+	}
+	// fmt.Println("done")
+}
+
+// vector subtract u - w
+func vdiff(u, w [2]int) [2]float64 {
+	x := float64(u[0] - w[0])
+	y := float64(u[1] - w[1])
+	return [2]float64{x, y}
+}
+
+// return representation of X in terms of basis vectors v,w
+func bary(v, w, X [2]float64) [2]float64 {
+	a := v[0]
+	b := w[0]
+	c := v[1]
+	d := w[1]
+	x := X[0]
+	y := X[1]
+	//a b   v    x
+	//c d * w  = y
+	det := 1 / (a*d - b*c)
+	cv := d*det*x - b*det*y
+	cw := -c*det*x + a*det*y
+	return [2]float64{cv, cw}
+	//x=c0v+c1w
+	//y=c2v+c3w
+
+	//v=c0x+c1y
+	//w=c2x+c3y
+	//r = c0/c2
+
+	//w*r = c0x + r*c3y
+
+	//w*r - v =  r*c3y-c1y
+	//s=(r*c3-c1)=((c0/c2)*c3-c1)
+
+	//****y= (-1/s)v+(r/s)w
+	//v=c0x+c1((-1/s)v+(r/s)w)
+	//x=(v-c1((-1/s)v+(r/s)w))/c0
+	//***x=(1/c0+c1/sc0)v+(r/sc0)w
+
+	//x=c0(vx*x + vy*y)+c1
+	//w=(x-c0(vx*x + vy*y))/c1
+	//x+y=c1v+c2w
+	//x+y=c1(vx*x + vy*y) + c2(wx*x+wy*y)
+	// V=vx*x + vy*y
+	// x=V-vy*y /vx
+	// y=W-vx*x / vw
+	//x=V-vy*(W-vx*x / vw) / vx
+	//-V=-x-vy*(W-vx*x / vw) / vx
 }
 
 /*
